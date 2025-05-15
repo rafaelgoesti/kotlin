ee2c39636255b15d2fb24b3aa6b6604689e43951
rafaelti <rafaelduartehd2030@gmail.com>
2025-05-14 21:51:41 -0300

    Formas

---
+++ abstracao/src/Forma.kt
@@ -0,0 +1,3 @@
+interface Forma {
+    fun calcularArea(): Double
+}
--- abstracao/src/Main.kt
+++ abstracao/src/Main.kt
@@ -22,6 +22,24 @@ class Leao : Animal{
     }
 }
 
+class Quadro(private val lado: Double) : Forma {
+    override fun calcularArea(): Double {
+        return lado * lado
+    }
+}
+
+class Retangulo(private val base: Double, private val altura: Double) : Forma {
+    override fun calcularArea(): Double {
+        return base * altura
+    }
+}
+
+class Circulo(private val raio: Double) : Forma {
+    override fun calcularArea(): Double {
+        return Math.PI * raio * raio
+    }
+}
+
 fun main(){
     // Pagamentos
     val pagamento1: Pagamento = CartaoCredito()
@@ -42,7 +60,15 @@ fun main(){
     // Funcionario
     val gerente = Gerente("Rafael Góes", 2000.0)
     val analista = Analista("Thiago", 3500.0)
-
     println("Bônus do gerente: R$ ${gerente.calcularBonus()}")
     println("Bônus da analista: R$ ${analista.calcularBonus()}")
+
+    //
+    val quadrado = Quadro(5.0)
+    val circulo = Circulo(3.0)
+    val retangulo = Retangulo(4.0, 6.0)
+
+    println("Área do quadrado: ${quadrado.calcularArea()}")
+    println("Área do círculo: ${circulo.calcularArea()}")
+    println("Área do retângulo: ${retangulo.calcularArea()}")
 }
